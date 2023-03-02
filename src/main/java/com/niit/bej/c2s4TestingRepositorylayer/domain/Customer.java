6d2ea172615cc7e741ef6b339bf499c42d3c73dc
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 22:34:10 +0530

    Created equals method and hash code for fields for the class Product

--- src/main/java/com/niit/bej/c2s4TestingRepositorylayer/domain/Customer.java
+++ src/main/java/com/niit/bej/c2s4TestingRepositorylayer/domain/Customer.java
@@ -3,6 +3,8 @@ package com.niit.bej.c2s4TestingRepositorylayer.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Document
 public class Customer {
     @Id
@@ -51,5 +53,18 @@ public class Customer {
 
     public void setCustomerProduct(Product customerProduct) {
         this.customerProduct = customerProduct;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return customerId == customer.customerId && customerPhoneNo == customer.customerPhoneNo && Objects.equals(customerName, customer.customerName) && Objects.equals(customerProduct, customer.customerProduct);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, customerName, customerPhoneNo, customerProduct);
     }
 }
