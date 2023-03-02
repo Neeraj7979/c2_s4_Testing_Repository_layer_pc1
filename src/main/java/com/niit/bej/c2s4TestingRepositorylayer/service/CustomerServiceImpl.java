49b0e42aade5c4e91b0e386cacfe8f781632be84
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 22:51:32 +0530

    Added customerRepository class dependency injection to the CustomerServiceImpl

--- src/main/java/com/niit/bej/c2s4TestingRepositorylayer/service/CustomerServiceImpl.java
+++ src/main/java/com/niit/bej/c2s4TestingRepositorylayer/service/CustomerServiceImpl.java
@@ -1,10 +1,18 @@
 package com.niit.bej.c2s4TestingRepositorylayer.service;
 
 import com.niit.bej.c2s4TestingRepositorylayer.domain.Customer;
+import com.niit.bej.c2s4TestingRepositorylayer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService{
+    CustomerRepository customerRepository;
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public Customer addCustomer(Customer customer) {
         return null;
