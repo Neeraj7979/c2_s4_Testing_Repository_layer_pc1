4b9cf896409aa637864ca439d304b7780ed19c15
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 22:29:46 +0530

    Created  fields for the class customer

--- src/main/java/com/niit/bej/c2s4TestingRepositorylayer/domain/Customer.java
+++ src/main/java/com/niit/bej/c2s4TestingRepositorylayer/domain/Customer.java
@@ -1,8 +1,14 @@
 package com.niit.bej.c2s4TestingRepositorylayer.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Customer {
+    @Id
+    private int customerId;
+    private String customerName;
+    private long customerPhoneNo;
+    private Product customerProduct;
 
 }
