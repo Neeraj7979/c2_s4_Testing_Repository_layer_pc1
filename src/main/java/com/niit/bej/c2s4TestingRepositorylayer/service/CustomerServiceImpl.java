2129f7d48fcfb3cb2f88acb08551b641dc40a55c
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 22:49:56 +0530

    Created class CustomerServiceImpl and implemented the methods of CustomerService interface

---
+++ src/main/java/com/niit/bej/c2s4TestingRepositorylayer/service/CustomerServiceImpl.java
@@ -0,0 +1,27 @@
+package com.niit.bej.c2s4TestingRepositorylayer.service;
+
+import com.niit.bej.c2s4TestingRepositorylayer.domain.Customer;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService{
+    @Override
+    public Customer addCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getCustomer() {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Integer id) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> findByProductName(String productName) {
+        return null;
+    }
+}
