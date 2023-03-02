0f05fd9e4b6b93a1a4cf65869c10dc0c9a918ad7
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 22:43:23 +0530

    Created query for finding product by product name

--- src/main/java/com/niit/bej/c2s4TestingRepositorylayer/repository/CustomerRepository.java
+++ src/main/java/com/niit/bej/c2s4TestingRepositorylayer/repository/CustomerRepository.java
@@ -2,8 +2,13 @@ package com.niit.bej.c2s4TestingRepositorylayer.repository;
 
 import com.niit.bej.c2s4TestingRepositorylayer.domain.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends MongoRepository<Customer,Integer> {
+    @Query("{'customerProduct.productName':?0}")
+    List<Customer>findByProductName(String productName);
 }
