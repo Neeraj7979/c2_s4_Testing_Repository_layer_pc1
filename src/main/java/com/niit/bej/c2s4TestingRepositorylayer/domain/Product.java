b5a8a70a09aa451f69e88654b813049bf916548b
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 22:36:52 +0530

    Created equals and hash code for the class Product

--- src/main/java/com/niit/bej/c2s4TestingRepositorylayer/domain/Product.java
+++ src/main/java/com/niit/bej/c2s4TestingRepositorylayer/domain/Product.java
@@ -1,5 +1,7 @@
 package com.niit.bej.c2s4TestingRepositorylayer.domain;
 
+import java.util.Objects;
+
 public class Product {
     private int productId;
     private String productName;
@@ -36,5 +38,27 @@ public class Product {
 
     public void setProductDescription(String productDescription) {
         this.productDescription = productDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Objects.equals(productName, product.productName) && Objects.equals(productDescription, product.productDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName, productDescription);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                '}';
     }
 }
