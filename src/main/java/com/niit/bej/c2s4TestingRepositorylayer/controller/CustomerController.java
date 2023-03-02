38ea929a394fcdabe95ac857082e92141f8af1c3
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 23:23:13 +0530

    Created class CustomerController

---
+++ src/main/java/com/niit/bej/c2s4TestingRepositorylayer/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.niit.bej.c2s4TestingRepositorylayer.controller;
+
+import com.niit.bej.c2s4TestingRepositorylayer.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+    private final CustomerService customerService;
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+}
